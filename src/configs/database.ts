418b405685604756ef33f6953a5f86345a53c634
pholpaphankorn <phol12711@outlook.com>
2023-08-05 21:04:03 +0700

    changed config

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -1,8 +1,8 @@
 import knex from 'knex';
 import * as fs from "fs";
 
-const readFileSync = (filename: string): string =>
-  fs.readFileSync(filename).toString("utf8");
+// const readFileSync = (filename: string): string =>
+//   fs.readFileSync(filename).toString("utf8");
 
 const databaseConnection= knex({
   client: 'mysql2',
@@ -10,9 +10,10 @@ const databaseConnection= knex({
       host: process.env.DATABASE_HOST,
       database: process.env.DATABASE_DB  ,
       user: process.env.DATABASE_USER ,
-      password: process.env.DATABASE_PASSWORD
-        ? readFileSync(process.env.DATABASE_PASSWORD)
-        : undefined,
+      // password: process.env.DATABASE_PASSWORD
+      //   ? readFileSync(process.env.DATABASE_PASSWORD)
+      //   : undefined,
+      password: process.env.DATABASE_PASSWORD,
       port: Number(process.env.DATABASE_PORT)
     // if you're not using docker compose for local development, this will default to 8080
     // to prevent non-root permission problems with 80. Dockerfile is set to make this 80
